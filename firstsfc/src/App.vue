acfbec931838be164e5aad8582cbb2e8cfbfde6d
Chelsea Hillary Nacalaban <cmnacalaban@student.apc.edu.ph>
2026-01-23 15:58:25 +0800

    TASK4

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,29 +3,10 @@ import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
 
 const instruments = ref([])
-const errorMsg = ref('') // Store error messages here
 
 async function getInstruments() {
-  try {
-    
-    const { data, error } = await supabase.from('instruments').select()
-    
-    
-    if (error) {
-        errorMsg.value = "Supabase Error: " + error.message
-        console.error(error)
-    } 
-    
-    else if (!data || data.length === 0) {
-        errorMsg.value = "Success, but database is empty! (Add rows in Supabase)"
-    } 
-    
-    else {
-        instruments.value = data
-    }
-  } catch (err) {
-      errorMsg.value = "System Error: " + err.message
-  }
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
 }
 
 onMounted(() => {
@@ -34,17 +15,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div style="padding: 20px; font-family: sans-serif;">
-    <h1>Debug Status:</h1>
-    
-    <div v-if="errorMsg" style="color: red; border: 1px solid red; padding: 10px;">
-      {{ errorMsg }}
-    </div>
-
-    <ul v-else>
-      <li v-for="instrument in instruments" :key="instrument.id">
-        {{ instrument.name }}
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
