b3e9632d345263fd761da6067f5b7616015b3a86
Chelsea Hillary Nacalaban <cmnacalaban@student.apc.edu.ph>
2026-01-23 14:26:11 +0800

    Try

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 
 
 import PersonalProfile from './components/PersonalProfile.vue'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
 
 const app = createApp(App)
 
 
 app.component('personal-profile', PersonalProfile)
+app.component('food-item', FoodItem)  
+app.component('food-item2', FoodItem2)
 
 app.mount('#app')
