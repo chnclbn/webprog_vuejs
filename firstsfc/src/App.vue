420455255194e684eb9489c7fbdda87f070301b5
Chelsea Hillary Nacalaban <cmnacalaban@student.apc.edu.ph>
2026-01-23 15:41:40 +0800

    Task 04 Create the Supabase client in your vercel hosted site

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,10 +3,29 @@ import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
 
 const instruments = ref([])
+const errorMsg = ref('') // Store error messages here
 
 async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
+  try {
+    
+    const { data, error } = await supabase.from('instruments').select()
+    
+    
+    if (error) {
+        errorMsg.value = "Supabase Error: " + error.message
+        console.error(error)
+    } 
+    
+    else if (!data || data.length === 0) {
+        errorMsg.value = "Success, but database is empty! (Add rows in Supabase)"
+    } 
+   
+    else {
+        instruments.value = data
+    }
+  } catch (err) {
+      errorMsg.value = "System Error: " + err.message
+  }
 }
 
 onMounted(() => {
@@ -15,7 +34,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <div style="padding: 20px; font-family: sans-serif;">
+    <h1>Debug Status:</h1>
+    
+    <div v-if="errorMsg" style="color: red; border: 1px solid red; padding: 10px;">
+      {{ errorMsg }}
+    </div>
+
+    <ul v-else>
+      <li v-for="instrument in instruments" :key="instrument.id">
+        {{ instrument.name }}
+      </li>
+    </ul>
+  </div>
 </template>
